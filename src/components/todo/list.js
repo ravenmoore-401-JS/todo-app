8155bf6e69ededf176a7fa78bb535ea71ee686a6
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-01-19 20:40:30 -0800

    tried some more bootstrap

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 
 export default function TodoList (props) {
 
   return (
-    <ul>
+    <ListGroup>
       {props.list.map(item => (
-        <li
+        <ListGroup.Item
           className={`complete-${item.complete.toString()}`}
           key={item._id}
         >
           <span onClick={() => props.handleComplete(item._id)}>
             {item.text}
           </span>
-        </li>
+        </ListGroup.Item>
       ))}
-    </ul>
+    </ListGroup>
   );
 }
